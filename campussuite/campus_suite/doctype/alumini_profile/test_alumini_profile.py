66ebb0cabc65d9c87b175a4674651041b6e16742
Abhishek Bankar <abhishekraje30@gmail.com>
2024-09-12 19:34:22 +0530

    Profile section added

---
+++ campussuite/campus_suite/doctype/alumini_profile/test_alumini_profile.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Abhishek Bankar and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAluminiProfile(FrappeTestCase):
+	pass
